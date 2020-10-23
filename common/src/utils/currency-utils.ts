1d0697cd5a08bb4d262c2c4e3e80146e5eebf8b0
Himesh Ramjee <himeshramjee@users.noreply.github.com>
2020-10-23 12:55:51 +0200

    Fix issue with formatCurrency() where it's internal use of non-breaking space with the Intl NumberFormatter was breaking the client code.

--- common/src/utils/currency-utils.ts
+++ common/src/utils/currency-utils.ts
@@ -1,16 +1,43 @@
 import { BadRequestError } from "../errors/bad-request-error";
 
-export const defaultCurrencySymbol = "R";
-
 export const removeCurrencyFormatting = (
-  price: string,
-  currencySymbol: string = defaultCurrencySymbol
+  price: string
 ): number => {
-  if (price.trim().length == 0) {
+  if (!price || price.trim().length == 0) {
     throw new BadRequestError("Price", "Price is required");
   }
 
-  price = price.replace(currencySymbol, "").replace(",", "");
+  var regexCurrencyValueOnly = /[^\d\.]/g;
+  price = price.replace(regexCurrencyValueOnly, "");
 
   return Number.parseFloat(price);
+};
+
+export const formatCurrency = (price: number, locale: string = "en-ZA", currencyCode: string = "ZAR"): string => {
+  if (!price) {
+    throw new BadRequestError("Price", "Price is required");
+  }
+
+  var formatter = new Intl.NumberFormat(locale, {
+    style: "currency",
+    currency: currencyCode
+  });
+
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/NumberFormat/formatToParts
+  // Holy smokes.
+  var priceString = formatter.formatToParts(price).map(({type, value}) => { 
+    switch (type) {
+      case "literal": {
+        // console.log(`Char code per formatter: ${value.charCodeAt(0)}.`);            // [Output] 160
+        // console.log(`Char code per normal string literal: ${" ".charCodeAt(0)}.`);  // [Output] 32
+
+        return ' '; // Magic line that bring back world peace.
+      };
+      case 'group': return `,`;
+      case 'decimal': return `.`;
+      default : return value; 
+    } 
+  }).reduce((string, part) => string + part);
+
+  return priceString;
 };
